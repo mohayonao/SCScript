6f86c785b5c5d01d22db18b26d2e8399593bc27b
mohayonao <mohayonao@gmail.com>
2014-04-27 08:20:19 +0900

    up

--- docs/report/plato/files/libs_installer_js/report.history.js
+++ docs/report/plato/files/libs_installer_js/report.history.js
@@ -1,1 +1,1 @@
-__history = [{"date":"Fri, 25 Apr 2014 10:28:30 GMT","sloc":2,"lloc":2,"functions":0,"deliveredBugs":0.004,"maintainability":152.65932560238386,"lintErrors":0,"difficulty":0.6666666666666666}]
+__history = [{"date":"Fri, 25 Apr 2014 10:28:30 GMT","sloc":2,"lloc":2,"functions":0,"deliveredBugs":0.004,"maintainability":152.65932560238386,"lintErrors":0,"difficulty":0.6666666666666666},{"date":"Sat, 26 Apr 2014 23:20:08 GMT","sloc":2,"lloc":2,"functions":0,"deliveredBugs":0.004,"maintainability":152.65932560238386,"lintErrors":0,"difficulty":0.6666666666666666}]

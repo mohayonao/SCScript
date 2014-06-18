b13c1c80b8181c0a44918f5de11edf757337724f
mohayonao <mohayonao@gmail.com>
2014-06-18 21:23:23 +0900

    use underscore on Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,8 @@
 module.exports = function(grunt) {
   "use strict";
 
+  var _ = require("underscore");
+
   grunt._loadNpmTasksIfNeeded = function(name) {
     if (!grunt._loadNpmTasksIfNeeded[name]) {
       grunt._loadNpmTasksIfNeeded[name] = true;
@@ -9,106 +11,90 @@ module.exports = function(grunt) {
   };
 
   grunt.file._expand = function() {
-    var result = [];
-    var map = grunt.file.readJSON("./tools/grunt-tasks/utils/files.json");
+    var dict = grunt.file.readJSON("tools/grunt-tasks/utils/files.json");
+    var list = grunt.file.expand(_.flatten(_.map(arguments, function(name) {
+      return dict[name] || name;
+    })));
 
-    [].slice.call(arguments).forEach(function(name) {
-      result.push.apply(result, map[name] || [ name ]);
-    });
-
-    result = grunt.file.expand(result);
-
-    Object.defineProperty(result, "applyFilter", {
+    Object.defineProperty(list, "applyFilter", {
       value: function(filter) {
         return applyFilter(this, filter);
       }
     });
 
-    return result;
+    return list;
   };
 
   var applyFilter = function(list, filter) {
-    var result = [];
-
-    if (filter) {
-      filter.split("+").forEach(function(filter) {
-        result.push.apply(result, list.filter(function(file) {
-          return file.indexOf(filter) !== -1;
-        }));
+    return !filter ? list : _.flatten(_.map(filter.split("+"), function(filter) {
+      return _.filter(list, function(file) {
+        return file.indexOf(filter) !== -1;
       });
-    } else {
-      result = list;
-    }
-
-    return result;
-  };
-
-  var toS = function(obj) {
-    return obj || "";
+    }));
   };
 
   var toTaskArgs = function(args) {
-    args = [].slice.call(args).map(toS).join(":");
-    if (args) {
-      args = ":" + args;
+    if (_.isUndefined(args)) {
+      return "";
+    }
+    if (_.isArguments(args)) {
+      return _.map(args, toTaskArgs).join(":");
     }
     return args;
   };
 
   grunt.initConfig({
-    pkg: grunt.file.readJSON("./package.json")
+    pkg: grunt.file.readJSON("package.json")
   });
 
-  if (process.argv.every(function(arg) {
-    return arg !== "--help";
-  })) {
+  if (!_.contains(process.argv, "--help")) {
     grunt.loadTasks("tools/grunt-tasks");
   }
 
   grunt.registerTask("default", "Start web server and run task whenever files changed.", [ "-connect", "-watch" ]);
 
   grunt.registerTask("typo", "Run the typo checker.", function() {
-    grunt.task.run("-typo" + toTaskArgs(arguments));
+    grunt.task.run("-typo:" + toTaskArgs(arguments));
   });
 
   grunt.registerTask("jscs", "Run the code style checker.", function() {
-    grunt.task.run("-jscs" + toTaskArgs(arguments));
+    grunt.task.run("-jscs:" + toTaskArgs(arguments));
   });
 
   grunt.registerTask("jshint", "Run the lint checker to detect errors and potential problems.", function() {
-    grunt.task.run("-jshint" + toTaskArgs(arguments));
+    grunt.task.run("-jshint:" + toTaskArgs(arguments));
   });
 
   grunt.registerTask("test", "Run node unit tests.", function() {
-    grunt.task.run("-mocha" + toTaskArgs(arguments));
+    grunt.task.run("-mocha:" + toTaskArgs(arguments));
   });
 
   grunt.registerTask("mocha", "Run node unit tests.", function() {
-    grunt.task.run("-mocha" + toTaskArgs(arguments));
+    grunt.task.run("-mocha:" + toTaskArgs(arguments));
   });
 
-  grunt.registerTask("cover", "Run node unit tests and report code coverage.", function(filter) {
-    grunt.task.run("-mocha:" + toS(filter) + ":nyan:lcov");
+  grunt.registerTask("cover", "Run node unit tests and create code coverage.", function(filter) {
+    grunt.task.run("-mocha:" + toTaskArgs(filter) + ":nyan:lcov");
   });
 
   grunt.registerTask("complexity", "Report code complexity.", function() {
-    grunt.task.run("-complexity" + toTaskArgs(arguments));
+    grunt.task.run("-complexity:" + toTaskArgs(arguments));
   });
 
   grunt.registerTask("complex", "Report code complexity.", function() {
-    grunt.task.run("-complexity" + toTaskArgs(arguments));
+    grunt.task.run("-complexity:" + toTaskArgs(arguments));
   });
 
   grunt.registerTask("lint", 'Alias for "typo", "jscs", "jshint" tasks.', function(filter) {
-    grunt.task.run("-typo:"   + toS(filter));
-    grunt.task.run("-jscs:"   + toS(filter));
-    grunt.task.run("-jshint:" + toS(filter));
+    grunt.task.run("-typo:"   + toTaskArgs(filter));
+    grunt.task.run("-jscs:"   + toTaskArgs(filter));
+    grunt.task.run("-jshint:" + toTaskArgs(filter));
   });
 
   grunt.registerTask("check", 'Alias for "lint", "test", "complex" tasks.', function(filter) {
-    grunt.task.run("lint:"        + toS(filter));
-    grunt.task.run("-mocha:"      + toS(filter) + ":nyan:text");
-    grunt.task.run("-complexity:" + toS(filter));
+    grunt.task.run("lint:"        + toTaskArgs(filter));
+    grunt.task.run("-mocha:"      + toTaskArgs(filter) + ":nyan:text");
+    grunt.task.run("-complexity:" + toTaskArgs(filter));
   });
 
   grunt.registerTask("travis"  , "Run tasks for Travis CI.", [ "-typo", "-jscs", "-jshint", "-mocha::list:lcovonly" ]);

cc5643ce113ad181e6987bdc6a02ca0727c030bb
mohayonao <mohayonao@gmail.com>
2014-06-23 17:12:48 +0900

    :muscle: implement klass.utils.toArray to convert to an array from arguments

--- src/sc/lang/klass/utils.js
+++ src/sc/lang/klass/utils.js
@@ -4,9 +4,13 @@
   require("./klass");
   require("./constructors");
 
+  var slice = Array.prototype.slice;
   var $nil = sc.lang.$.nil;
 
   sc.lang.klass.utils = {
+    toArray: function(args) {
+      return slice.call(args);
+    },
     newCopyArgs: function(that, dict) {
       var instance = new that.__Spec();
       Object.keys(dict).forEach(function(key) {

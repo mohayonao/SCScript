7f3a83ff8b3a7684a1ff5fca79b2839ba8cd6eb9
mohayonao <mohayonao@gmail.com>
2014-06-30 21:17:22 +0900

    :sparkles: add tests for ProgramCodeGen

---
+++ src/sc/lang/compiler/codegen/program_test.js
@@ -0,0 +1,57 @@
+(function() {
+  "use strict";
+
+  require("./installer");
+
+  var Syntax = sc.lang.compiler.Syntax;
+  var Token = sc.lang.compiler.Token;
+  var CodeGen = sc.lang.compiler.CodeGen;
+
+  describe("sc.lang.compiler.CodeGen", function() {
+    describe("Program", function() {
+      sc.test.codegen().each([
+        {
+          code: "<empty>",
+          expected: "",
+          ast: {
+            type: Syntax.Program,
+            body: []
+          }
+        },
+        {
+          code: "nil; nil",
+          expected: "SCScript(function($) { $.Nil(); return $.Nil(); });",
+          ast: {
+            type: Syntax.Program,
+            body: [
+              {
+                type: Syntax.Literal,
+                value: "nil",
+                valueType: Token.NilLiteral
+              },
+              {
+                type: Syntax.Literal,
+                value: "nil",
+                valueType: Token.NilLiteral
+              }
+            ]
+          }
+        },
+      ]);
+      it("bare option", function() {
+        var codegen = new CodeGen(null, { bare: true });
+        var test = codegen.generate({
+          type: Syntax.Program,
+          body: [
+            {
+              type: Syntax.Literal,
+              value: "nil",
+              valueType: Token.NilLiteral
+            }
+          ]
+        }).replace(/[\s\n]+/g, " ");
+        expect(test).to.eql("(function($) { return $.Nil(); })");
+      });
+    });
+  });
+})();

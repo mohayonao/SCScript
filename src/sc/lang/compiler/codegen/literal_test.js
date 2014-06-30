5aaa78424d92495e2fbfe8e5d0e167308dca2cbc
mohayonao <mohayonao@gmail.com>
2014-06-30 21:08:35 +0900

    :sparkles: add test for LiteralCodeGen

---
+++ src/sc/lang/compiler/codegen/literal_test.js
@@ -0,0 +1,87 @@
+(function() {
+  "use strict";
+
+  require("./installer");
+
+  var Syntax = sc.lang.compiler.Syntax;
+  var Token = sc.lang.compiler.Token;
+
+  describe("sc.lang.compiler.CodeGen", function() {
+    describe("EnvironmentExpression", function() {
+      sc.test.codegen().each([
+        {
+          code: "nil",
+          expected: "$.Nil()",
+          ast: {
+            type: Syntax.Literal,
+            value: "nil",
+            valueType: Token.NilLiteral
+          }
+        },
+        {
+          code: "true",
+          expected: "$.True()",
+          ast: {
+            type: Syntax.Literal,
+            value: "true",
+            valueType: Token.TrueLiteral
+          }
+        },
+        {
+          code: "false",
+          expected: "$.False()",
+          ast: {
+            type: Syntax.Literal,
+            value: "false",
+            valueType: Token.FalseLiteral
+          }
+        },
+        {
+          code: "10",
+          expected: "$.Integer(10)",
+          ast: {
+            type: Syntax.Literal,
+            value: "10",
+            valueType: Token.IntegerLiteral
+          }
+        },
+        {
+          code: "10.0",
+          expected: "$.Float(10.0)",
+          ast: {
+            type: Syntax.Literal,
+            value: "10.0",
+            valueType: Token.FloatLiteral
+          }
+        },
+        {
+          code: "\\10",
+          expected: "$.Symbol('10')",
+          ast: {
+            type: Syntax.Literal,
+            value: "10",
+            valueType: Token.SymbolLiteral
+          }
+        },
+        {
+          code: '"10"',
+          expected: "$.String('10')",
+          ast: {
+            type: Syntax.Literal,
+            value: "10",
+            valueType: Token.StringLiteral
+          }
+        },
+        {
+          code: "$a",
+          expected: "$.Char('a')",
+          ast: {
+            type: Syntax.Literal,
+            value: "a",
+            valueType: Token.CharLiteral
+          }
+        },
+      ]);
+    });
+  });
+})();

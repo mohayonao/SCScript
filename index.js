0f5b51bd5b56dbda02e417759b5241b6b27696b9
mohayonao <mohayonao@gmail.com>
2014-07-04 12:25:24 +0900

    :dash: rename the main file, index.js -> src/scscript.js

--- index.js
+++ index.js
@@ -1,18 +1,1 @@
-"use strict";
-
-var fs = require("fs");
-
-var pkg = JSON.parse(fs.readFileSync("./package.json"));
-var saved = global.sc;
-
-module.exports = global.sc = { VERSION: pkg.version };
-
-require("./src/const");
-require("./src/sc/");
-require("./src/sc/classlib/");
-
-if (typeof saved !== "undefined") {
-  global.sc = saved;
-} else {
-  delete global.sc;
-}
+module.exports = require("./src/scscript");

81c0622991c45bd9050c0b94c9cbcaa0d7e2429f
mohayonao <mohayonao@gmail.com>
2014-07-13 07:00:52 +0900

    :sparkles: fix test-utils

--- tools/test-utils.js
+++ tools/test-utils.js
@@ -1,4 +1,4 @@
-/* globals _: true, it: true, expect: true, esprima: true */
+/* globals _: true, it: true, expect: true, esprima: true, escodegen: true */
 (function(sc) {
   "use strict";
 
@@ -7,80 +7,82 @@
 
   var $ = sc.lang.$;
 
+  var SCObject = $("Object");
   var SCEnvironment = $("Environment");
   var SCPseq = $("Pseq");
   var SCRoutine = $("Routine");
 
-  sc.test = function(callback) {
-    return function() {
-      SCEnvironment.new().push();
-      callback.apply(this);
-      SCEnvironment.pop();
-    };
-  };
+  function nth(list, index) {
+    return list[index];
+  }
 
-  function toSCObject(obj, opts) {
-    var $ = sc.lang.$;
+  function first(list) {
+    return nth(list, 0);
+  }
 
-    if (isSCObject(obj)) {
-      return obj;
-    }
-    if (Array.isArray(obj)) {
-      return $.Array(obj.map(toSCObject));
-    }
-    if (obj === null) {
-      return $.Nil();
-    }
-    if (typeof obj === "undefined") {
-      return undefined;
-    }
-    if (typeof obj === "number") {
-      if ((obj|0) === obj) {
-        return $.Integer(obj);
-      } else {
-        return $.Float(obj);
-      }
-    }
-    if (typeof obj === "boolean") {
-      return $.Boolean(obj);
-    }
-    if (typeof obj === "string") {
-      if (obj.length === 2 && obj.charAt(0) === "$") {
-        return $.Char(obj.charAt(1));
-      }
-      if (obj.charAt(0) === "\\") {
-        return $.Symbol(obj.substr(1));
-      }
-      return $.String(obj);
-    }
-    if (typeof obj === "function") {
-      return $.Function(function() {
-        return [ obj ];
-      }, typeof opts === "string" ? opts : undefined);
-    }
+  function second(list) {
+    return nth(list, 1);
+  }
 
-    return obj;
+  function third(list) {
+    return nth(list, 2);
+  }
+
+  function isNaN(num) {
+    return typeof num === "number" && global.isNaN(num);
+  }
+
+  function isFinite(num) {
+    return typeof num === "number" && global.isFinite(num);
+  }
+
+  function isDictionary(obj) {
+    return _.isObject(obj) && obj.constructor === Object;
+  }
+
+  function isInteger(obj) {
+    return _.isNumber(obj) && Math.floor(obj) === Math.ceil(obj);
+  }
+
+  function isChar(obj) {
+    return _.isString(obj) && /^\$.$/.test(obj);
+  }
+
+  function isSymbol(obj) {
+    return _.isString(obj) && obj.charAt(0) === "\\";
+  }
+
+  function isSCObject(obj) {
+    return _.isObject(obj) && typeof obj._ !== "undefined";
+  }
+
+  function capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  }
+
+  function js(str) {
+    return ast2js(js2ast(str));
+  }
+
+  function js2ast(js) {
+    return esprima.parse(js);
+  }
+
+  function ast2js(ast) {
+    return escodegen.generate(ast);
   }
 
   function toString(obj) {
     var str = JSON.stringify(obj) || (typeof obj);
+
     if (str.length > 2) {
       str = str.replace(/([{[,])/g, "$1 ").replace(/([\]}])/g, " $1");
     }
+
     return str;
   }
 
-  function isDictionary(obj) {
-    return obj && obj.constructor === Object;
-  }
-
-  function isSCObject(obj) {
-    return obj && typeof obj._ !== "undefined";
-  }
-
   function typeOf(obj, guess) {
-    var type;
-
     if (isSCObject(obj)) {
       if (obj.__tag === sc.TAG_BOOL) {
         return "SCBoolean";
@@ -92,38 +94,96 @@
       return typeOf(toSCObject(obj));
     }
 
-    if (obj === null) {
+    if (_.isNull(obj)) {
       return "JSNull";
     }
 
-    if (Array.isArray(obj)) {
+    if (_.isArray(obj)) {
       return "JSArray";
     }
 
-    type = typeof obj;
-    return "JS" + type.charAt(0).toUpperCase() + type.slice(1);
+    return "JS" + capitalize(typeof obj);
   }
 
-  sc.test.testCase = function(context, cases, opts) {
-    var expect = global.expect;
-    var methodName    = context.test.title;
-    var isClassMethod = (methodName.charAt(0) === ".");
+  function toSCObject(obj, opts) {
+    var $ = sc.lang.$;
 
+    if (isSCObject(obj)) {
+      return obj;
+    }
+
+    if (_.isArray(obj)) {
+      return $.Array(obj.map(toSCObject));
+    }
+
+    if (_.isNull(obj)) {
+      return $.Nil();
+    }
+
+    if (_.isUndefined(obj)) {
+      return undefined;
+    }
+
+    if (isInteger(obj)) {
+      return $.Integer(obj);
+    }
+
+    if (_.isNumber(obj)) {
+      return $.Float(obj);
+    }
+
+    if (_.isBoolean(obj)) {
+      return $.Boolean(obj);
+    }
+
+    if (isChar(obj)) {
+      return $.Char(obj.charAt(1));
+    }
+
+    if (isSymbol(obj)) {
+      return $.Symbol(obj.substr(1));
+    }
+
+    if (_.isString(obj)) {
+      return $.String(obj);
+    }
+
+    if (_.isFunction(obj)) {
+      return $.Function(function() {
+        return [ obj ];
+      }, typeof opts === "string" ? opts : undefined);
+    }
+
+    return obj;
+  }
+
+  sc.test = function(callback) {
+    return function() {
+      SCEnvironment.new().push();
+      callback.apply(this);
+      SCEnvironment.pop();
+    };
+  };
+
+  sc.test.testCase = function(context, cases, opts) {
     opts = opts || {};
+
+    var methodName    = context.test.title;
+    var isClassMethod = methodName.charAt(0) === ".";
+
     methodName = methodName.substr(1).replace(/\..*$/, "");
 
-    if (typeof opts.randSeed === "number") {
+    if (isInteger(opts.randSeed)) {
       sc.libs.random.setSeed(opts.randSeed);
     }
 
     cases.forEach(function(items) {
-      var instance, test, raw, desc, expected;
-      var source, args, result, error, type;
+      var source, args, result, error;
 
       if (Array.isArray(items)) {
-        source = items[0];
-        args   = items[1];
-        result = items[2];
+        source = first(items);
+        args   = second(items);
+        result = third(items);
         if (result instanceof Error) {
           error = result.message;
         }
@@ -134,10 +194,11 @@
         error  = items.error;
       }
 
-      desc = sc.libs.strlib.format(
+      var desc = sc.libs.strlib.format(
         "#{0}.#{1}(#{2})", toString(source), methodName, toString(args).slice(2, -2)
       );
 
+      var instance;
       if (isClassMethod) {
         instance = context.createInstance().class();
       } else {
@@ -150,21 +211,26 @@
         }).to.throw(error);
       }
 
-      test = instance[methodName].apply(instance, args.map(toSCObject));
+      var test = instance[methodName].apply(instance, args.map(toSCObject));
+
       if (result === context) {
         // expect to return this like `function() { return this; }`
-        expect(test).withMessage(desc).to.equal(instance);
+        expect(test, desc).to.equal(instance);
       } else {
-        expected = toSCObject(result);
-        type     = typeOf(expected);
+        var expected = toSCObject(result);
+        var type     = typeOf(expected);
+
         if (result) {
-          result   = result.valueOf();
-          if (type === "SCSymbol" && result.charAt(0) === "\\") {
+          result = result.valueOf();
+          if (type === "SCSymbol" && isSymbol(result)) {
             result = result.substr(1);
-          } else if (type === "SCChar" && result.charAt(0) === "$") {
+          } else if (type === "SCChar" && isChar(result)) {
             result = result.substr(1);
           }
         }
+
+        var expects = expect(test, desc);
+
         switch (type) {
         case "SCInteger":
         case "SCSymbol":
@@ -172,113 +238,103 @@
         case "SCNil":
         case "SCBoolean":
         case "SCString":
-          expect(test).withMessage(desc)
-            .to.a(type).that.equals(result);
+          expects.to.a(type).that.equals(result);
           break;
         case "SCFloat":
+          expects = expects.to.a("SCFloat");
           if (isFinite(result) && opts.closeTo) {
-            expect(test).withMessage(desc)
-              .to.a("SCFloat").that.is.closeTo(result, opts.closeTo);
+            expects.that.is.closeTo(result, opts.closeTo);
           } else if (isNaN(result)) {
-            expect(test).withMessage(desc)
-              .to.a("SCFloat").that.is.nan; // jshint ignore: line
-          } else { // Infinity
-            expect(test).withMessage(desc)
-              .to.a("SCFloat").that.equals(result);
+            expects.that.is.NaN; // jshint ignore: line
+          } else {
+            expects.that.equals(result);
           }
           break;
         case "SCArray":
-          expect(test).withMessage(desc)
-            .to.a("SCArray").that.eqls(result);
+          expects.to.a("SCArray").that.deep.equals(result);
           break;
         case "SCFunction":
-          expect(test).withMessage(desc)
-            .to.equal(result);
+          expects.to.equal(result);
           break;
         default:
-          expect(test.valueOf()).withMessage(desc).to.eql(result);
+          expect(test.valueOf(), desc).to.eql(result);
         }
       }
 
       // test for destructive
-      raw = instance.valueOf();
-      if (Array.isArray(raw)) {
-        if (items.after) {
-          expect(raw).withMessage(desc + ": after").to.eql(items.after);
-        } else {
-          expect(raw).withMessage(desc + ": after").to.eql(
-            source.valueOf().slice().map(function(x) {
-              return x && x.valueOf();
-            })
-          );
-        }
-      } else if (isDictionary(raw)) {
-        if (items.after) {
-          expect(raw).withMessage(desc + ": after").to.eql(items.after);
-        }
+      var raw   = instance.valueOf();
+      var after = items.after || null;
+
+      if (_.isNull(after) && _.isArray(raw)) {
+        after = source.valueOf().slice().map(function(a) {
+          return a && a.valueOf();
+        });
+      }
+
+      if (after) {
+        expect(raw, desc + ": after").to.deep.equal(after);
       }
     });
   };
 
-  var prev = null;
-  function setSingletonMethod(instance, className, methodName) {
-    var method;
-
-    if (prev) {
-      delete prev.instance[prev.methodName];
-      prev = null;
+  function setUniqMethod(instance, className, methodName) {
+    if (setUniqMethod.prev) {
+      delete setUniqMethod.prev.instance[setUniqMethod.prev.methodName];
+      setUniqMethod.prev = null;
     }
 
-    method = sc.lang.klass._classes[className].__Spec.prototype[methodName];
-    Object.defineProperty(instance, methodName, {
-      value: method, configurable: true
+    var method = sc.lang.klass._classes[className].__Spec.prototype[methodName];
+
+    Object.defineProperty(instance, methodName, { value: method, configurable: true });
+
+    setUniqMethod.prev = { instance: instance, methodName: methodName };
+  }
+
+  function createSCObjectFrom(defaults) {
+    var instance = SCObject.new();
+
+    _.chain(defaults).keys().each(function(key) {
+      Object.defineProperty(instance, key, { value: defaults[key] });
     });
-    prev = {
-      instance: instance, methodName: methodName
-    };
+
+    return instance;
   }
 
   sc.test.object = function(source, opts) {
-    var instance, matches;
+    var instance;
 
     if (isSCObject(source)) {
       instance = source;
-      matches = /^([A-Z]\w*)#([a-z]\w*|[-+*\/%<=>!?&|@]+)/.exec(opts);
-      if (matches) {
-        setSingletonMethod(instance, matches[1], matches[2]);
-      }
-    } else if (typeof source === "undefined") {
-      instance = sc.lang.klass._classes.Object.new();
-    } else if (isDictionary(source)) {
-      instance = sc.lang.klass._classes.Object.new();
-      Object.keys(source).forEach(function(key) {
-        Object.defineProperty(instance, key, {
-          value: source[key]
-        });
-      });
+    } else if (_.isUndefined(source) || isDictionary(source)) {
+      instance = createSCObjectFrom(source);
     } else {
       instance = toSCObject(source, opts);
     }
+
+    var matches = /^([A-Z]\w*)#([a-z]\w*|[-+*\/%<=>!?&|@]+)/.exec(opts);
+    if (matches) {
+      setUniqMethod(instance, second(matches), third(matches));
+    }
+
     instance.__testid = instance.__hash;
 
     return instance;
   };
 
   sc.test.func = function() {
-    var seed, fn;
+    var seed = Math.random();
 
-    seed = Math.random();
-    fn = function() {
+    function func() {
       return seed;
-    };
-    fn.__seed = seed;
+    }
+    func.__seed = seed;
 
-    return fn;
+    return func;
   };
 
   sc.test.routine = function(source, opts) {
-    if (Array.isArray(source)) {
-      if (source.length) {
+    if (_.isArray(source)) {
+      if (!_.isEmpty(source)) {
         return SCPseq.new(toSCObject(source), toSCObject(opts || 1)).asStream();
       }
       return SCRoutine.new(toSCObject(function() {
@@ -293,78 +349,97 @@
   sc.test.PASS = "@PASS";
 
   sc.test.compile = function(methodName, opts) {
-    var func = function(items) {
-      var code = items[0], expected = items[1];
-      var chain, desc;
+
+    function func(items) {
+      var code     = first(items);
+      var expected = second(items);
+
       if (expected === sc.test.PASS) {
         return;
       }
+
+      var chain, desc;
       if (expected === sc.test.OK) {
-        chain = "not";
-        desc = "ok";
+        chain    = "not";
+        desc     = "ok";
         expected = null;
       } else {
         chain = "to";
-        desc = "throw " + expected;
+        desc  = "throw " + expected;
       }
+
       it(code + ": " + desc, function() {
         var lexer  = new sc.lang.compiler.Lexer(code);
         var parser = new sc.lang.compiler.Parser(null, lexer);
+
         expect(function() {
           parser[methodName](opts);
         }).to[chain].throw(expected);
       });
-    };
+    }
+
     func.each = function(suites) {
       return _.each(_.pairs(suites), func);
     };
+
     return func;
   };
 
   sc.test.parse = function(methodName, opts) {
-    var func = function(items) {
-      var code = items[0], expected = items[1];
+
+    function func(items) {
+      var code     = first(items);
+      var expected = second(items);
+
       it(code, function() {
         var lexer  = new sc.lang.compiler.Lexer(code, { loc: true, range: true });
         var parser = new sc.lang.compiler.Parser(null, lexer);
         var ast = parser[methodName](opts);
+
         expect(ast).to.eql(expected);
       });
-    };
+    }
+
     func.each = function(suites) {
       return _.each(_.pairs(suites), func);
     };
+
     return func;
   };
 
   sc.test.codegen = function() {
-    var func = function(items) {
+
+    function func(items) {
       var code = items.code;
       var ast = items.ast;
       var expected = items.expected;
       var before   = items.before;
+
       it(code, function() {
-        var expectedAST = esprima.parse(expected);
         var codegen = new sc.lang.compiler.CodeGen();
+
         if (before) {
           before(codegen);
         }
-        var compiled  = codegen.generate(ast);
-        var actualAST = esprima.parse(compiled);
-        expect(actualAST).to.eql(expectedAST);
+
+        var compiled = codegen.generate(ast);
+
+        expect(js(compiled)).to.equal(js(expected));
       });
-    };
+    }
+
     func.each = function(suites) {
       return _.each(suites, func);
     };
+
     return func;
   };
 
-  // for chai
+  // chai extending
   global.chai.use(function(chai, utils) {
-    var assert$proto = chai.Assertion.prototype;
+    var Assertion = chai.Assertion;
 
-    utils.overwriteChainableMethod(assert$proto, "a", function(_super) {
+    Assertion.overwriteChainableMethod("a", function(_super) {
       return function(type) {
         if (!/^(SC|JS)/.test(String(type))) {
           return _super.apply(this, arguments);
@@ -372,16 +447,18 @@
 
         var object = utils.flag(this, "object");
         var actual = typeOf(object);
+
         this.assert(
           actual === type,
           "expected " + actual + " to be a " + type,
           "expected " + actual + " not to be a " + type
         );
-        if (object && typeof object._ !== "undefined") {
+
+        if (isSCObject(object)) {
           object = object.valueOf();
         }
 
-        return new chai.Assertion(object, utils.flag(this, "message"));
+        return new Assertion(object, utils.flag(this, "message"));
       };
     }, function() {
       return function() {
@@ -389,32 +466,38 @@
       };
     });
 
-    utils.overwriteMethod(assert$proto, "closeTo", function(_super) {
+    Assertion.addMethod("clsTo", function(expected, delta, msg) {
+      var actual = utils.flag(this, "object");
+
+      if (![ actual, expected ].every(Array.isArray)) {
+        return this.closeTo(expected, delta, msg);
+      }
+
+      msg = msg || "";
+      for (var i = 0, imax = Math.max(actual.length, expected.length); i < imax; ++i) {
+        new Assertion(actual[i]).deep.closeTo(expected[i], delta, msg + "[" + i + "]");
+      }
+
+      return this;
+    });
+
+    Assertion.overwriteMethod("closeTo", function(_super) {
       return function(expected, delta, msg) {
-        if (utils.flag(this, "deep")) {
+        var actual = utils.flag(this, "object");
+
+        if ( utils.flag(this, "deep") && [ actual, expected ].every(Array.isArray) ) {
           return this.clsTo(expected, delta, msg);
         }
+
         return _super.apply(this, arguments);
       };
     });
 
-    utils.addMethod(assert$proto, "clsTo", function(expected, delta, msg) {
-      var obj = utils.flag(this, "object");
-      if (!Array.isArray(obj) || !Array.isArray(expected)) {
-        return this.closeTo(expected, delta, msg);
-      }
-      msg = msg || "";
-      for (var i = 0, imax = Math.max(obj.length, expected.length); i < imax; ++i) {
-        new chai.Assertion(obj[i]).closeTo(expected[i], delta, msg + "[" + i + "]");
-      }
-      return this;
-    });
-
-    utils.addMethod(assert$proto, "withMessage", function() {
+    Assertion.addMethod("withMessage", function() {
       utils.flag(this, "message", sc.libs.strlib.format.apply(null, arguments));
     });
 
-    utils.addMethod(assert$proto, "calledLastIn", function(seed) {
+    Assertion.addMethod("calledLastIn", function(seed) {
       var expected = utils.flag(this, "object").__seed;
       this.assert(
         seed === expected || (seed && seed.__seed === expected),
@@ -424,7 +507,7 @@
       );
     });
 
-    utils.addProperty(assert$proto, "doNothing", function() {
+    Assertion.addProperty("doNothing", function() {
       this.assert(
         utils.flag(this, "object") === sc.lang.$.DoNothing,
         "expected #{this} to do nothing",
@@ -433,10 +516,9 @@
       );
     });
 
-    utils.addProperty(assert$proto, "NaN", function() {
-      var num = utils.flag(this, "object");
+    Assertion.addProperty("NaN", function() {
       this.assert(
-        typeof num === "number" && isNaN(num),
+        isNaN(utils.flag(this, "object")),
         "expected #{this} to be NaN",
         "expected #{this} to be not NaN",
         this.negate ? false : true

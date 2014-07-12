53002ba478e01a6ad151035523e6a22548a019f3
mohayonao <mohayonao@gmail.com>
2014-07-13 05:05:57 +0900

    :sparkles: add test

--- src/sc/lang/compiler/parser/braces_test.js
+++ src/sc/lang/compiler/parser/braces_test.js
@@ -3,13 +3,12 @@ describe("sc.lang.compiler.Parser", function() {
 
   var Syntax = sc.lang.compiler.Syntax;
   var Message = sc.lang.compiler.Message;
-  var Parser = sc.lang.compiler.Parser;
-  var Lexer = sc.lang.compiler.Lexer;
   var strlib = sc.libs.strlib;
 
   describe("parseBraces", function() {
     sc.test.compile(this.title).each({
       "{}": sc.test.OK,
+      "{: a, a <- 3 }": sc.test.OK,
       "[]": strlib.format(Message.UnexpectedToken, "["),
     });
 
@@ -24,28 +23,6 @@ describe("sc.lang.compiler.Parser", function() {
         }
       }
     });
-
-    // TODO: fix later implement parseGeneratorExpression
-    describe("parse generator", function() {
-      it("parse", sinon.test(function() {
-        var p = new Parser(null, new Lexer("{: a, a <- a }"));
-
-        this.stub(p, "parseGeneratorExpression", sc.test.func());
-
-        var test = p.parseBraces();
-        expect(p.parseGeneratorExpression).to.be.calledLastIn(test);
-      }));
-      it("throw error when blockList", sinon.test(function() {
-        var p = new Parser(null, new Lexer("{: a, a <- a }"));
-
-        this.stub(p, "parseGeneratorExpression");
-
-        expect(function() {
-          p.parseBraces({ blockList: true });
-        }).to.throw(strlib.format(Message.UnexpectedToken, ":"));
-
-        expect(p.parseGeneratorExpression).to.be.not.called;
-      }));
-    });
   });
+
 });
